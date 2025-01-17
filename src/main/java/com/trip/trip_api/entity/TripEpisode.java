a57edc01b086b075adc856c176d4e765f018e25c
Oliver Cook <65789247+ollie-cook@users.noreply.github.com>
2025-01-17 15:08:22 +0000

    Result gets returned as JSON

--- src/main/java/com/trip/trip_api/entity/TripEpisode.java
+++ src/main/java/com/trip/trip_api/entity/TripEpisode.java
@@ -6,9 +6,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "trip_episodes")
+@AllArgsConstructor
+@NoArgsConstructor
 public class TripEpisode {
   @Id
   @Column(name="id")
@@ -20,12 +24,36 @@ public class TripEpisode {
   @Column(name="date_created")
   private Timestamp dateCreated;
 
-  @Override
-  public String toString() {
-      return "TripEpisode{" +
-              "id=" + id +
-              ", title='" + title + '\'' +
-              ", dateCreated=" + dateCreated +
-              '}';
-  }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    } 
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Timestamp getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Timestamp dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    @Override
+    public String toString() {
+        return "TripEpisode{" +
+            "id=" + id +
+            ", title='" + title + '\'' +
+            ", dateCreated=" + dateCreated +
+            '}';
+    }
 }
