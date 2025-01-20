e63c9205d08e59755148e992c55d817843f65804
Oliver Cook <65789247+ollie-cook@users.noreply.github.com>
2025-01-20 09:14:01 +0000

    Added new fields

--- src/main/java/com/trip/trip_api/entity/TripEpisode.java
+++ src/main/java/com/trip/trip_api/entity/TripEpisode.java
@@ -1,6 +1,7 @@
 package com.trip.trip_api.entity;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -21,6 +22,15 @@ public class TripEpisode {
     @Column(name="title")
     private String title;
 
+    @Column(name="summary")
+    private String summary;
+
+    @Column(name="url")
+    private String url;
+
+    @Column(name="uuid")
+    private UUID uuid;
+
     @Column(name="date_created")
     private LocalDate dateCreated;
 
@@ -38,6 +48,30 @@ public class TripEpisode {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUrl(UUID uuid) {
+        this.uuid = uuid;
     }
 
     public LocalDate getDateCreated() {
